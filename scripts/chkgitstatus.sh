92c57adb8c8742c028856e3e62f548e539bf4379
Mark Millard <msm@wizzerworks.com>
2016-08-09 13:22:29 -0600

    New tool for detecting which projects have modifications that need to be committed.

---
+++ scripts/chkgitstatus.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# For each Android Studio project, determine if there is anything that has been modified.
+# Note that this is not an elegant solution since we are assuming that the name of the
+# projects are the same as the github repository.
+
+for i in core-math core-runtime parts-base parts-mrefs parts-roles parts-props parts-sets parts-stages parts-actors;
+do
+    # Process Android project.
+    echo $i;
+    cd $i;
+
+    # Use git status to flush state where diff-index was detecting files that do not
+    # need to be committed (jar files touched by stagelibs.py script). Not sure why this works,
+    # there may be an option on diff-index that could be used instead.
+    git status > /dev/null;
+
+    if git diff-index --name-status --exit-code HEAD;
+    then
+        echo "No update required.";
+    else
+        echo "Update required.";
+    fi
+
+    cd ..
+    echo ""
+done
